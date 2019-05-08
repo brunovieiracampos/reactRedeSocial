d4815216939cf743bef693838f010284e02b4c04
Bruno Campos <bruno.campos@cbyk.com.br>
2019-05-08 19:54:31 -0300

    create function add comment

--- instacbyk/src/componentes/Foto.js
+++ instacbyk/src/componentes/Foto.js
@@ -24,7 +24,7 @@ class FotoInfo extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { likers: this.props.foto.likers };
+        this.state = { likers: this.props.foto.likers, comentarios: this.props.foto.comentarios };
     }
 
     componentWillMount() {
@@ -43,6 +43,15 @@ class FotoInfo extends Component {
                     const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
                     this.setState({ likers: novosLikers });
                 }
+            }
+        });
+
+
+        Pubsub.subscribe('novos-comentarios', (topico, infoComentario) => {
+
+            if (this.props.foto.id === infoComentario.fotoId) {
+                const novosComentarios = this.state.comentarios.concat(infoComentario.novoComentario);
+                this.setState({ comentarios: novosComentarios });
             }
         });
     }
@@ -67,7 +76,7 @@ class FotoInfo extends Component {
 
                 <ul className="foto-info-comentarios">
                     {
-                        this.props.foto.comentarios.map(comentario => {
+                        this.state.comentarios.map(comentario => {
                             return (<li className="comentario" key={comentario.id}>
                                 <Link className="foto-info-autor" to={`/timeline/${comentario.login}`}>{comentario.login}</Link>
                                 {comentario.texto}
@@ -104,12 +113,34 @@ class FotoAtualizacoes extends Component {
         });
     }
 
+    comenta(event) {
+        event.preventDefault();
+
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({ texto: this.comentario.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch(`https://instalura-api.herokuapp.com/api/fotos/${this.props.foto.id}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Não foi possível realizar o comentário');
+            }
+        }).then(novoComentario => {
+            Pubsub.publish('novos-comentarios', { fotoId: this.props.foto.id, novoComentario });
+        });
+    }
+
     render() {
         return (
             <section className="fotoAtualizacoes">
                 <a onClick={this.like.bind(this)} className={this.state.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
-                <form className="fotoAtualizacoes-form">
-                    <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+                <form className="fotoAtualizacoes-form" onSubmit={this.comenta.bind(this)}>
+                    <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comentario = input} />
                     <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
                 </form>
             </section>
